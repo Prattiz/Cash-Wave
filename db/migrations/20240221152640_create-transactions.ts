68dc4d866c9dccc0a073d7a5a1955daa04bfad68
Thiago <prattithiago5@gmail.com>
2024-02-21 13:22:11 -0300

    ADD TABLE TRANSACTIONS

--- db/migrations/20240221152640_create-transactions.ts
+++ db/migrations/20240221152640_create-transactions.ts
@@ -2,10 +2,20 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+
+    await knex.schema.createTable('transactions', (table) => {
+
+        table.uuid('id').primary()
+        table.text('title').notNullable()
+        table.decimal('amount', 10, 2).notNullable()
+        table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable()
+    })
 }
 
 
 
 export async function down(knex: Knex): Promise<void> {
+
+    await knex.schema.dropTable('transactions');
 }
 
